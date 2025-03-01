ac120f2567afd33ac43bc29d12a4dba46779fff0
pawanjoshi05 <139532941+pawanjoshi05@users.noreply.github.com>
2025-03-01 04:37:21 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,9 @@
+let name="prateek"
+console.log (name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.trim())
+console.log(name.charAt(2))
+console.log(name.indexOf("a"))
+console.log(name.substring(0,3))
+console.log(name.split(","))
