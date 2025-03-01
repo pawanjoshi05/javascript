362ef23e1e8e16ad924e6adc723b63231f614b9c
pawanjoshi05 <139532941+pawanjoshi05@users.noreply.github.com>
2025-03-01 09:00:50 +0000

    ARRAYS

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,22 @@
+// const jsuser={
+//     name: "prateek",
+//     age:22,
+//     location:"greater Noida",
+
+// }
+// console.log(jsuser.name)
+// console.log(jsuser["name"])
+
+// ******************************************************
+const obj = {
+    1:"a",
+    2:"b",
+    3:"c"
+}
+const obj2={
+    4:"d",
+    5:"e"
+}
+// *************************IMPORTANT***************
+const obj3={...obj,...obj2}//SPREAD METHDD
+console.log(obj3)
