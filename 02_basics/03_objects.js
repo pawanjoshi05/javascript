a19c63c499283f7bbdb4f0b11b71a9e329a7cf3b
pawanjoshi05 <139532941+pawanjoshi05@users.noreply.github.com>
2025-03-01 09:54:51 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,22 +1,45 @@
-// const jsuser={
-//     name: "prateek",
-//     age:22,
-//     location:"greater Noida",
+// // const jsuser={
+// //     name: "prateek",
+// //     age:22,
+// //     location:"greater Noida",
 
+// // }
+// // console.log(jsuser.name)
+// // console.log(jsuser["name"])
+
+// // ******************************************************
+// const obj = {
+//     1:"a",
+//     2:"b",
+//     3:"c"
 // }
-// console.log(jsuser.name)
-// console.log(jsuser["name"])
+// const obj2={
+//     4:"d",
+//     5:"e"
+// }
+// // *************************IMPORTANT***************
+// const obj3={...obj,...obj2}//SPREAD METHDD
+// console.log(obj3)
 
-// ******************************************************
-const obj = {
-    1:"a",
-    2:"b",
-    3:"c"
+// const course = {
+//     coursename:"prateek",
+//     age:22,
+//     price:2222,
+//     codeinstructor:"prateek"
+// }
+// const{codeinstructor:instructor}=course  //kaha se value extract krni h ..humghe krni h course instructor se
+// console.log(instructor)
+//*********************************************** */
+// Basic Destructuring: { key1, key2 } = object
+// Renaming Variables: { key1: newName }
+// Default Values: { key1 = defaultValue }
+// Nested Destructuring: { key1: { nestedKey } }
+// Object as Function Parameters:
+// javascript
+// Copy code
+
+{
+    "name":"prateek",
+    "price":220,
+    "coursename" : "js"
 }
-const obj2={
-    4:"d",
-    5:"e"
-}
-// *************************IMPORTANT***************
-const obj3={...obj,...obj2}//SPREAD METHDD
-console.log(obj3)
