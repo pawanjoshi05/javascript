33587ca6bc4d467ae162f69b6657704be665ed73
pawanjoshi05 <139532941+pawanjoshi05@users.noreply.github.com>
2025-03-01 09:04:33 +0000

    ARRAYS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,6 @@
+const myArr = [1,3,4,5,true,"prateek"]
+// ****************************************************
+myArr.push(5)
+myArr.push(4)
+myArr.push(18)
+console.log(myArr)
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,7 @@
+// const heros=["ironman","thor","captainamerica"]
+// const dc=["superman","batman"]
+// heros.push(dc)
+// console.log(heros)
+// const newheros=heros.concat(dc)
+// console.log(newheros)
+  
